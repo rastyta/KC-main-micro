5a44b26eb084a633075428c75dd7255647ad72b9
jmoreno <jmoreno@guidewire.com>
2023-06-08 17:00:21 +0200

    Commit message:

--- src/main/java/com/rasty/kc/controllers/kcMainController.java
+++ src/main/java/com/rasty/kc/controllers/kcMainController.java
@@ -1,10 +1,13 @@
 package com.rasty.kc.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 @RestController
 public class kcMainController {
@@ -14,7 +17,9 @@ public class kcMainController {
 
     @GetMapping("/requestDemomicro")
     public ResponseEntity<String> makeRequestToApp2() {
-        String demomicroUrl = discoveryClient.getInstances("kc-service").get(0).getUri().toString();
+        List<ServiceInstance> urls = discoveryClient.getInstances("kc-service");
+        System.out.println("********************** url: " + urls.size());
+        String demomicroUrl = urls.get(0).getUri().toString();
         System.out.println("********************** url: " + demomicroUrl);
         String url = demomicroUrl + "/airlines/airline_10";
 

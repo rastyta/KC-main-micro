9873ba93c6afca5f8ef497b1446a6f8b2b5a1bff
jmoreno <jmoreno@guidewire.com>
2023-06-12 11:14:46 +0200

    Commit message:

--- src/main/java/com/rasty/kc/HelloController.java
+++ src/main/java/com/rasty/kc/HelloController.java
@@ -1,19 +1,23 @@
 package com.rasty.kc;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
+	@Value("${message.value}")
+	private String message;
+
 	@GetMapping("/")
 	public String index() {
 		return "Hello Rasty from main-micro! **";
 	}
 
-    @GetMapping("/name")
+    @GetMapping("/message")
 	public String getName() {
-		return "main-micro here !! **";
+		return message;
 	}
 
 }
